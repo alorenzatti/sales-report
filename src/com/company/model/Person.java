6c8031d86c2842ff39b27dd1df37e6f9b70b58d1
Alexandre Lorenzatti <alorenza@gmail.com>
2018-05-12 23:25:21 -0300

    Created Person type.

---
+++ src/com/company/model/Person.java
@@ -0,0 +1,12 @@
+package com.company.model;
+
+public interface Person {
+	
+	public String getName();
+	
+	public void setName(String name);
+	
+	public String getRegistration();
+	
+	public void setRegistration(String registration);
+}
